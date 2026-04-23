27a4539f886370608245e8350edde7bbe59c0a73
janumaruku <jean-marc.kottin@epitech.eu>
2026-04-23 20:05:33 +0200

    chore(ci): RAYT-11 #comment Add JIRA issue transition script for CI integration

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -7,16 +7,8 @@
 
 #include <iostream>
 
-constexpr int EPITECH_FAIL = 84;
-
-int main(void)
+int main()
 {
-    // try {
-    //     Raytracer r;
-    //     r.run();
-    // } catch (const std::exception &e) {
-    //     return EPITECH_FAIL;
-    // }
     std::cout << "Hello world!" << std::endl;
     return EXIT_SUCCESS;
 }
