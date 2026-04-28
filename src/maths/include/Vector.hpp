d803b9ecf4e8e57febbe4a9e03400b3b6e7a5035
janumaruku <jean-marc.kottin@epitech.eu>
2026-04-28 21:05:53 +0200

    feat(maths): RAYT-24 #comment Refactor Vector class to enhance functionality, improve naming consistency, and add new methods

--- src/maths/include/Vector.hpp
+++ src/maths/include/Vector.hpp
@@ -11,52 +11,71 @@
 #include <array>
 #include <iostream>
 
-template<std::size_t N, typename T = double, bool PrecomputeNorm = false>
+template <std::size_t N, typename T = double, bool PrecomputeNorm = false>
 class Vector {
 public:
-    using iterator = typename std::array<T, N>::iterator;
+    using iterator       = typename std::array<T, N>::iterator;
     using const_iterator = typename std::array<T, N>::const_iterator;
 
-    Vector() = default;
-    Vector(std::array<T, N> &values);
+    Vector();
 
-    using UnitVector = Vector<N, T, PrecomputeNorm>;
-    UnitVector normalize() const;
+    explicit Vector(std::array<T, N> &values);
 
-    T getNorm() const;
-    T computeNorm() const;
-    void loadNorm();
+    explicit Vector(std::array<T, N> &&values);
 
-    T calculateAngle(const Vector<N> &other) const;
+    using UnitVector = Vector;
 
-    T& operator[](std::size_t i);
-    const T& operator[](std::size_t i) const;
+    [[nodiscard]] UnitVector normalize() const;
+
+    [[nodiscard]] T getLength() const;
+
+    T length() const;
+
+    void loadLength();
+
+    [[nodiscard]] T calculateAngle(const Vector<N> &other) const;
+
+    T &operator[](std::size_t idx);
+
+    const T &operator[](std::size_t idx) const;
 
     Vector operator+(const Vector &other) const;
+
     Vector operator-(const Vector &other) const;
 
     // dot product
     T operator*(const Vector &other) const;
+    [[nodiscard]] T dot(const Vector &other) const;
+
     Vector operator*(T scalar) const;
 
     Vector operator/(T scalar) const;
+
     Vector operator-() const;
 
     bool operator==(const Vector &other) const;
+
     bool operator!=(const Vector &other) const;
 
     // norm comparaison
     bool operator<(const Vector &other) const;
+
     bool operator<=(const Vector &other) const;
+
     bool operator>(const Vector &other) const;
+
     bool operator>=(const Vector &other) const;
 
     iterator begin() noexcept;
+
     const_iterator begin() const noexcept;
+
     const_iterator cbegin() const noexcept;
 
     iterator end() noexcept;
+
     const_iterator end() const noexcept;
+
     const_iterator cend() const noexcept;
 
 private:
@@ -64,22 +83,65 @@ private:
     T _norm;
 };
 
-template<std::size_t N, typename T, bool PrecomputeNorm>
+template <std::size_t N, typename T, bool PrecomputeNorm>
 using UnitVector = Vector<N, T, PrecomputeNorm>;
 
-using Vector2f = Vector<2, float>;
-using Vector3f = Vector<3, float>;
+using Vector2f    = Vector<2, float>;
+using Vector3f    = Vector<3, float>;
 using UnitVector2 = Vector<2>;
 using UnitVector3 = Vector<3>;
 
-template<typename T>
+template <typename T>
 Vector<3> cross(Vector<3> a, Vector<3> b);
 
+template <std::size_t N, typename T, bool PrecomputeNorm>
+Vector<N + 1, T, PrecomputeNorm> IncreaseDimension(
+    const Vector<N, T, PrecomputeNorm> &a);
 
-template<std::size_t N, typename T, bool PrecomputeNorm>
-Vector<N + 1, T, PrecomputeNorm> IncreaseDimension(const Vector<N, T, PrecomputeNorm> &a);
+template <std::size_t N, typename T, bool PrecomputeNorm>
+std::ostream &operator<<(std::ostream &o,
+    const Vector<N, T, PrecomputeNorm> &vector);
 
-template<std::size_t N, typename T, bool PrecomputeNorm>
-std::ostream &operator<<(std::ostream &o, const Vector<N, T, PrecomputeNorm> &vector);
+template <std::size_t N, typename T, bool PrecomputeNorm>
+bool operator==(const Vector<N, T, PrecomputeNorm> &lhs,
+    const Vector<N, T, PrecomputeNorm> &rhs)
+{
+    return lhs.operator==(rhs);
+}
+
+template <std::size_t N, typename T, bool PrecomputeNorm>
+bool operator!=(const Vector<N, T, PrecomputeNorm> &lhs,
+    const Vector<N, T, PrecomputeNorm> &rhs)
+{
+    return lhs.operator!=(rhs);
+}
+
+template <std::size_t N, typename T, bool PrecomputeNorm>
+bool operator<(const Vector<N, T, PrecomputeNorm> &lhs,
+    const Vector<N, T, PrecomputeNorm> &rhs)
+{
+    return lhs.operator<(rhs);
+}
+
+template <std::size_t N, typename T, bool PrecomputeNorm>
+bool operator>(const Vector<N, T, PrecomputeNorm> &lhs,
+    const Vector<N, T, PrecomputeNorm> &rhs)
+{
+    return lhs.operator>(rhs);
+}
+
+template <std::size_t N, typename T, bool PrecomputeNorm>
+bool operator<=(const Vector<N, T, PrecomputeNorm> &lhs,
+    const Vector<N, T, PrecomputeNorm> &rhs)
+{
+    return lhs.operator<=(rhs);
+}
+
+template <std::size_t N, typename T, bool PrecomputeNorm>
+bool operator>=(const Vector<N, T, PrecomputeNorm> &lhs,
+    const Vector<N, T, PrecomputeNorm> &rhs)
+{
+    return lhs.operator>=(rhs);
+}
 
 #include "Vector.tpp"
