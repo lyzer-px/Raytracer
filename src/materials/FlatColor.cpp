3de0f4db0368330588174c65c4acbe119f0634a9
janumaruku <jean-marc.kottin@epitech.eu>
2026-05-01 18:13:16 +0200

    feat(materials): RAYT-32 #comment Add FlatColor material implementation with CMake integration

---
+++ src/materials/FlatColor.cpp
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2026
+** raytracer
+** File description:
+** FlatColor
+*/
+
+#include "include/FlatColor.hpp"
+
+namespace raytracer {
+namespace material {
+FlatColor::FlatColor(const Color &color): _color{color}
+{}
+
+Color FlatColor::getColor(const shape::SurfaceInteraction &si) const // NOLINT
+{
+    return _color;
+}
+} // material
+} // raytracer
