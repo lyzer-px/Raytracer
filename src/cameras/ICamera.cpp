64f15b23f1082e63b24e84d8f668bfd23f76c545
janumaruku <jean-marc.kottin@epitech.eu>
2026-05-02 11:08:44 +0200

    feat(cameras): RAY-36 #comment Add ICamera interface and CMake setup for camera module

---
+++ src/cameras/ICamera.cpp
@@ -0,0 +1,12 @@
+/*
+** EPITECH PROJECT, 2026
+** raytracer
+** File description:
+** ICamera
+*/
+
+#include "include/ICamera.hpp"
+
+namespace raytracer {
+namespace camera {} // camera
+} // raytracer
---
+++ src/cameras/include/ICamera.hpp
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2026
+** raytracer
+** File description:
+** ICamera
+*/
+
+#ifndef RAYTRACER_ICAMERA_HPP
+#define RAYTRACER_ICAMERA_HPP
+
+namespace raytracer {
+namespace camera {
+
+class ICamera {
+
+};
+
+} // camera
+} // raytracer
+
+#endif //RAYTRACER_ICAMERA_HPP
