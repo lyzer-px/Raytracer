8687cfa5d38537f3dcbe48cd471589db7f28e820
janumaruku <jean-marc.kottin@epitech.eu>
2026-05-02 13:07:03 +0200

    feat(maths): RAY-37 #comment Refactor Vector classes for improved readability and consistency

--- src/maths/include/Vector.hpp
+++ src/maths/include/Vector.hpp
@@ -14,29 +14,43 @@ template <template<typename>class Derived, std::size_t N, typename T = double>
 class Vector {
 public:
     Vector() = default;
+
     Vector(std::initializer_list<T> values);
 
     [[nodiscard]] bool hasNaN() const;
 
     const T &x() const;
+
     T &x();
+
     const T &y() const;
+
     T &y();
-    const T &z() const requires (N >= 3);
-    T &z()       requires (N >= 3);
+
+    const T &z() const
+        requires (N >= 3);
+
+    T &z()
+        requires (N >= 3);
 
     [[nodiscard]] Derived<T> normalize() const;
-    Derived<T> cross(const Derived<T> &other) const requires (N == 3); // cross product only defined for 3D vectors
-    Derived<T> cross(Derived<T> &other) const requires (N == 3);
+
+    Derived<T> cross(const Derived<T> &other) const
+        requires (N == 3); // cross product only defined for 3D vectors
+    Derived<T> cross(Derived<T> &other) const
+        requires (N == 3);
 
     [[nodiscard]] T length() const;
+
     [[nodiscard]] T calculateAngle(const Derived<T> &other) const;
 
     Derived<T> operator+(const Derived<T> &other) const;
+
     Derived<T> operator-(const Derived<T> &other) const;
 
     // dot product
     T operator*(const Derived<T> &other) const;
+
     T dot(const Derived<T> &other) const;
 
     // scalar multiplication
@@ -54,34 +68,39 @@ public:
 
     // norm comparaison
     bool operator<(const Derived<T> &other) const;
+
     bool operator<=(const Derived<T> &other) const;
+
     bool operator>(const Derived<T> &other) const;
+
     bool operator>=(const Derived<T> &other) const;
 
-    const std::array<T, N>& data() const;
+    const std::array<T, N> &data() const;
 
 protected:
     std::array<T, N> _data;
 };
 
-template<typename T>
-class Vector3 : public Vector<Vector3, 3, T> {
+template <typename T>
+class Vector3: public Vector<Vector3, 3, T> {
 public:
     Vector3();
+
     Vector3(T x, T y, T z) noexcept;
 
-    template<typename U>
-    explicit Vector3(const Point3<U>& v); // conversion constructor
+    template <typename U>
+    explicit Vector3(const Point3<U> &v); // conversion constructor
 };
 
-template<typename T>
+template <typename T>
 class Vector2: public Vector<Vector2, 2, T> {
 public:
     Vector2();
+
     Vector2(T x, T y) noexcept;
 
-    template<typename U>
-    explicit Vector2(const Point2<U>& v); // conversion constructor
+    template <typename U>
+    explicit Vector2(const Point2<U> &v); // conversion constructor
 };
 
 using Vector2d = Vector2<double>;
