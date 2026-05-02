2046a538c34517c9afad787964eeac664b7fc1ca
janumaruku <jean-marc.kottin@epitech.eu>
2026-05-02 14:51:13 +0200

    feat(integrator): RAY-39 #comment Add IIntegrator interface with core methods for rendering and radiance computation

---
+++ src/integrators/include/IIntegrator.hpp
@@ -0,0 +1,31 @@
+/*
+** EPITECH PROJECT, 2026
+** raytracer
+** File description:
+** IIntegrator
+*/
+
+#ifndef RAYTRACER_IINTEGRATOR_HPP
+#define RAYTRACER_IINTEGRATOR_HPP
+
+#include "Color.hpp"
+#include "ICamera.hpp"
+#include "Ray.hpp"
+#include "Scene.hpp"
+
+namespace raytracer {
+namespace integrator {
+class IIntegrator {
+public:
+    virtual ~IIntegrator() = default;
+
+    virtual Color Li(const Ray &ray, const scene::Scene &scene,
+        int depth) const = 0;
+
+    virtual void render(const scene::Scene &scene,
+        const camera::ICamera &camera/*, Film &film*/) const = 0;
+};
+} // integrator
+} // raytracer
+
+#endif //RAYTRACER_IINTEGRATOR_HPP
