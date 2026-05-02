574b766047325d481b21c708a2ed47995cd75b14
janumaruku <jean-marc.kottin@epitech.eu>
2026-05-02 11:23:16 +0200

    feat(cameras): RAY-37 #comment Update PerspectiveCamera implementation and fix include paths in CMake setup

--- src/cameras/include/PerspectiveCamera.hpp
+++ src/cameras/include/PerspectiveCamera.hpp
@@ -8,13 +8,25 @@
 #ifndef RAYTRACER_PERSPECTIVECAMERA_HPP
 #define RAYTRACER_PERSPECTIVECAMERA_HPP
 
+#include "ICamera.hpp"
+#include "maths_types.hpp"
+
 namespace raytracer {
 namespace camera {
+class PerspectiveCamera: public ICamera {
+public:
+    PerspectiveCamera(const Point3f &position, const Point3f &target,
+        const Vector3f &up, float fovDegrees, float aspectRatio);
 
-class PerspectiveCamera {
+    [[nodiscard]] Ray
+        generateRay(const float &u, const float &v) const override;
 
+private:
+    Point3f _origin;
+    Point3f _lowerLeft;
+    Vector3f _horizontal;
+    Vector3f _vertical;
 };
-
 } // camera
 } // raytracer
 
