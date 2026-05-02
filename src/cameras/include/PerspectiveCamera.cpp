eaa7c76b3f6f00dd21effe0ce678ac496ca4bcc8
janumaruku <jean-marc.kottin@epitech.eu>
2026-05-02 11:18:00 +0200

    feat(cameras): RAY-37 #comment Add PerspectiveCamera class with header and CMake integration

---
+++ src/cameras/include/PerspectiveCamera.cpp
@@ -0,0 +1,12 @@
+/*
+** EPITECH PROJECT, 2026
+** raytracer
+** File description:
+** PerspectiveCamera
+*/
+
+#include "PerspectiveCamera.hpp"
+
+namespace raytracer {
+namespace camera {} // camera
+} // raytracer
---
+++ src/cameras/include/PerspectiveCamera.hpp
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2026
+** raytracer
+** File description:
+** PerspectiveCamera
+*/
+
+#ifndef RAYTRACER_PERSPECTIVECAMERA_HPP
+#define RAYTRACER_PERSPECTIVECAMERA_HPP
+
+namespace raytracer {
+namespace camera {
+
+class PerspectiveCamera {
+
+};
+
+} // camera
+} // raytracer
+
+#endif //RAYTRACER_PERSPECTIVECAMERA_HPP
