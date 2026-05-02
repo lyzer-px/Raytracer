74d45d4eb5fcada1d596f96987bfd0bafabc7b72
janumaruku <jean-marc.kottin@epitech.eu>
2026-05-02 13:17:20 +0200

    feat(cameras): RAY-37 #comment Introduce generateRay method

--- src/cameras/PerspectiveCamera.cpp
+++ src/cameras/PerspectiveCamera.cpp
@@ -22,11 +22,19 @@ PerspectiveCamera::PerspectiveCamera(const Point3d &position,
     const Vector3d trueUp = right.cross(forward);
 
     const Vector3d offsetRight = right * halfWidth;
-    const Vector3d offsetUp = trueUp * halfHeight;
+    const Vector3d offsetUp    = trueUp * halfHeight;
 
-    _lowerLeft = _origin + (forward - offsetRight - offsetUp);
+    _lowerLeft  = _origin + (forward - offsetRight - offsetUp);
     _horizontal = right * (2.0 * halfWidth);
-    _vertical = trueUp * (2.0 * halfHeight);
+    _vertical   = trueUp * (2.0 * halfHeight);
+}
+
+Ray PerspectiveCamera::generateRay(const float &u, const float &v) const
+{
+    const Point3d target = _lowerLeft + _horizontal * u + _vertical * v;
+    const Vector3d direction = (target - _origin).normalize();
+
+    return Ray{_origin, direction};
 }
 } // camera
 } // raytracer
