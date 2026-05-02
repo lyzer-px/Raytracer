7fbffd0a098c5df87b343fa1d1855dc0bef27255
janumaruku <jean-marc.kottin@epitech.eu>
2026-05-02 10:33:06 +0200

    feat(lights): RAY-33 #comment Add AmbientLight class definition and implementation

---
+++ src/lights/AmbientLight.cpp
@@ -0,0 +1,12 @@
+/*
+** EPITECH PROJECT, 2026
+** raytracer
+** File description:
+** AmbientLight
+*/
+
+#include "AmbientLight.hpp"
+
+namespace raytracer {
+namespace light {} // light
+} // raytracer
---
+++ src/lights/include/AmbientLight.hpp
@@ -0,0 +1,31 @@
+/*
+** EPITECH PROJECT, 2026
+** raytracer
+** File description:
+** AmbientLight
+*/
+
+#ifndef RAYTRACER_AMBIENTLIGHT_HPP
+#define RAYTRACER_AMBIENTLIGHT_HPP
+
+#include "ILight.hpp"
+
+namespace raytracer {
+namespace light {
+
+class AmbientLight: public ILight {
+public:
+    explicit AmbientLight(const Color &radiance);
+
+    [[nodiscard]] LightSample sample(const Point3f &p) const override;
+
+    [[nodiscard]] bool isDelta() const override;
+
+private:
+    Color _radiance;
+};
+
+} // light
+} // raytracer
+
+#endif //RAYTRACER_AMBIENTLIGHT_HPP
