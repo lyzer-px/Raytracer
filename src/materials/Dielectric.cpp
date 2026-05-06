b49a3468cc45cee5e81f4bc5a73c12a9f705cbbc
janumaruku <jean-marc.kottin@epitech.eu>
2026-05-06 05:46:34 +0200

    feat(material): RAY-75 #comment Create class Dielectric for transparent materials

---
+++ src/materials/Dielectric.cpp
@@ -0,0 +1,12 @@
+/*
+** EPITECH PROJECT, 2026
+** raytracer
+** File description:
+** Dielectric
+*/
+
+#include "include/Dielectric.hpp"
+
+namespace raytracer {
+namespace material {} // material
+} // raytracer
