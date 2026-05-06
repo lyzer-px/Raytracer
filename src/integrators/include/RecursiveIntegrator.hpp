118da86677d4394a1c484de07656fc6c31ff1cff
janumaruku <jean-marc.kottin@epitech.eu>
2026-05-06 11:41:37 +0200

    feat(integrator): RAY-77 #comment Implement recursive integrator

---
+++ src/integrators/include/RecursiveIntegrator.hpp
@@ -0,0 +1,34 @@
+/*
+** EPITECH PROJECT, 2026
+** raytracer
+** File description:
+** RecursiveIntegrator
+*/
+
+#ifndef RAYTRACER_RECURSIVEINTEGRATOR_HPP
+#define RAYTRACER_RECURSIVEINTEGRATOR_HPP
+
+#include "IIntegrator.hpp"
+
+namespace raytracer {
+namespace integrator {
+
+class RecursiveIntegrator: public IIntegrator {
+public:
+    explicit RecursiveIntegrator(const int &maxDepth = 5, int samplesPerPixel = 32);
+
+    maths::Color accumulatedRadiance(const maths::Ray &ray,
+        const scene::Scene &scene, int depth) const override;
+
+    void render(const scene::Scene &scene, const camera::ICamera &camera,
+        camera::Film &film) const override;
+
+private:
+    int _maxDepth        = 5;
+    int _samplesPerPixel = 32;
+};
+
+} // integrator
+} // raytracer
+
+#endif //RAYTRACER_RECURSIVEINTEGRATOR_HPP
