cca850890f8576c09ef64f4e190f26ea49febbf0
janumaruku <jean-marc.kottin@epitech.eu>
2026-05-15 21:04:56 +0200

    feat: RAY-56 #comment Make point light store intensity as Vector and fix scene lightning issue

--- src/lights/PointLight.cpp
+++ src/lights/PointLight.cpp
@@ -13,11 +13,11 @@ namespace raytracer::light {
 
 PointLight::PointLight(const nlohmann::json &config):
     _position{config.at("position").get<maths::Point3d>()},
-    _intensity{config.at("intensity").get<maths::Color>()}
+    _intensity{config.at("intensity").get<maths::Vector3d>()}
 {}
 
 PointLight::PointLight(
-    const maths::Point3d &position, const maths::Color &intensity):
+    const maths::Point3d &position, const maths::Vector3d &intensity):
     _position{position}, _intensity{intensity}
 {}
 
@@ -25,11 +25,14 @@ LightSample PointLight::sample(const maths::Point3d &p) const
 {
     const maths::Vector3d toLight = _position - p;
     const double dist             = toLight.length();
-    const maths::Vector3d wi      = toLight.normalize();
+    const maths::Vector3d wi      = -toLight.normalize();
+    const double dist2            = dist * dist;
 
     return LightSample{
         .wi       = wi,
-        .radiance = _intensity / (dist * dist),
+        .radiance = maths::Color{_intensity.x() / dist2,
+                                 _intensity.y() / dist2,
+                                 _intensity.z() / dist2},
         .distance = dist};
 }
 
--- src/lights/include/PointLight.hpp
+++ src/lights/include/PointLight.hpp
@@ -15,7 +15,7 @@ namespace raytracer::light {
 class PointLight: public ILight {
 public:
     explicit PointLight(const nlohmann::json &config);
-    PointLight(const maths::Point3d &position, const maths::Color &intensity);
+    PointLight(const maths::Point3d &position, const maths::Vector3d &intensity);
 
     [[nodiscard]] LightSample sample(const maths::Point3d &p) const override;
 
@@ -25,7 +25,7 @@ public:
 
 private:
     maths::Point3d _position;
-    maths::Color   _intensity;
+    maths::Vector3d _intensity;
 };
 
 } // namespace raytracer::light
